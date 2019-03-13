PixelExperience/WeatherClient

Add gzip support

app/src/main/java/org/pixelexperience/weather/client/SunriseSunsetRestApi.java
@@ -74,6 +74,7 @@ class SunriseSunsetRestApi {
                 .followRedirects(false)
                 .followSslRedirects(false)
                 .addNetworkInterceptor(REWRITE_RESPONSE_INTERCEPTOR)
+                .addInterceptor(new Utils.GzipRequestInterceptor())
                 .addInterceptor(OFFLINE_INTERCEPTOR)
                 .cache(new Cache(new File(mContext.getCacheDir(),
                         "SunriseSunsetRestApiCache"), 10 * 1024 * 1024))

app/src/main/java/org/pixelexperience/weather/client/WeatherChannelApi.java
@@ -147,6 +147,7 @@ public class WeatherChannelApi implements OnFailureListener, OnCanceledListener 
                 .followRedirects(false)
                 .followSslRedirects(false)
                 .addNetworkInterceptor(REWRITE_RESPONSE_INTERCEPTOR)
+                .addInterceptor(new Utils.GzipRequestInterceptor())
                 .addInterceptor(OFFLINE_INTERCEPTOR)
                 .cache(new Cache(new File(mContext.getCacheDir(),
                         "WeatherChannelApiCache"), 10 * 1024 * 1024))

BootleggersROM/packages_apps_WeatherClient

src/java/com/android/providers/weather/SunriseSunsetRestApi.java
@@ -92,6 +92,7 @@ class SunriseSunsetRestApi {
                 .followRedirects(false)
                 .followSslRedirects(false)
                 .addNetworkInterceptor(REWRITE_RESPONSE_INTERCEPTOR)
+                .addInterceptor(new Utilities.GzipRequestInterceptor())
                 .addInterceptor(OFFLINE_INTERCEPTOR)
                 .cache(new Cache(new File(mContext.getCacheDir(),
                         "SunriseSunsetRestApiCache"), 10 * 1024 * 1024))

src/java/com/android/providers/weather/WeatherChannelApi.java
@@ -164,6 +164,7 @@ public class WeatherChannelApi implements OnFailureListener, OnCanceledListener 
                 .followRedirects(false)
                 .followSslRedirects(false)
                 .addNetworkInterceptor(REWRITE_RESPONSE_INTERCEPTOR)
+                .addInterceptor(new Utilities.GzipRequestInterceptor())
                 .addInterceptor(OFFLINE_INTERCEPTOR)
                 .cache(new Cache(new File(mContext.getCacheDir(),
                         "WeatherChannelApiCache"), 10 * 1024 * 1024))